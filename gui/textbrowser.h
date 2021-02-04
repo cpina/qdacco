d070683ae2c1200d11f6c196c09bc3b5b25753c2
Carles Pina i Estany <carles@pina.cat>
2021-02-04 00:34:27 +0000

    Beter formatting, const methods, const parameters

--- gui/textbrowser.h
+++ gui/textbrowser.h
@@ -11,7 +11,7 @@
  * any later version.
  *
  * qdacco is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
@@ -36,137 +36,137 @@
 
 class TextBrowser : public QTextBrowser
 {
-	Q_OBJECT
-	private:
-		QProcess *m_browser_process;
-		QString m_browser_location;
-		int m_festival_enable;
+    Q_OBJECT
+private:
+    QProcess *m_browser_process;
+    QString m_browser_location;
+    int m_festival_enable;
 
-		QMainWindow *m_parent;
-		QPoint m_qpoint;
+    QMainWindow *m_parent;
+    QPoint m_qpoint;
 
-		int getCharPosition() {
-			QTextCursor qtc;
-			qtc = this->textCursor();
-			return qtc.position();
-		}
+    int getCharPosition() const {
+        QTextCursor qtc;
+        qtc = this->textCursor();
+        return qtc.position();
+    }
 
-		void selectWord() {
-			//Warn: only valid after m_qupoint has been setted up
-			QTextCursor qtc;
-			QString s;
+    void selectWord() {
+        //Warn: only valid after m_qupoint has been setted up
+        QTextCursor qtc;
+        QString s;
 
-			if (!this->textCursor().hasSelection()) {
-				qtc = this->cursorForPosition(m_qpoint);
-				qtc.select(QTextCursor::WordUnderCursor);
-				this->setTextCursor(qtc);
-			}
-		}
+        if (!this->textCursor().hasSelection()) {
+            qtc = this->cursorForPosition(m_qpoint);
+            qtc.select(QTextCursor::WordUnderCursor);
+            this->setTextCursor(qtc);
+        }
+    }
 
-	public:
-        TextBrowser(QWidget *&) {}
+public:
+    TextBrowser(QWidget *&) {}
 
-		void setFather(QMainWindow *m) {
-			m_parent=m;
-		}
-		void setProcess(QProcess *p_) {
-			m_browser_process=p_;
-		}
-		void setBrowser(QString browser_) {
-			m_browser_location=browser_;
-		}
+    void setFather(QMainWindow *m) {
+        m_parent=m;
+    }
+    void setProcess(QProcess *process) {
+        m_browser_process=process;
+    }
+    void setBrowser(const QString& browser) {
+        m_browser_location=browser;
+    }
 
-		void setParent(QMainWindow *m) {
-			m_parent=m;
-		}
-		void setFestivalEnable(int f) {
-			m_festival_enable=f;
-		}
-	
-		virtual void setSource(const QUrl& url) {
-            m_browser_process->setProgram(m_browser_location);
-            m_browser_process->setArguments(QStringList() << url.toString());
-            m_browser_process->startDetached();
-            Auxiliar::debug("Executing: "+m_browser_location);
-		}
+    void setParent(QMainWindow *mainWindow) {
+        m_parent=mainWindow;
+    }
+    void setFestivalEnable(int f) {
+        m_festival_enable=f;
+    }
 
-		virtual void contextMenuEvent(QContextMenuEvent *e) {
-			int language;
-			m_qpoint=e->pos();
-			
-			selectWord();
+    virtual void setSource(const QUrl& url) {
+        m_browser_process->setProgram(m_browser_location);
+        m_browser_process->setArguments(QStringList() << url.toString());
+        m_browser_process->startDetached();
+        Auxiliar::debug("Executing: "+m_browser_location);
+    }
 
-			QMenu *menu = createStandardContextMenu();
-			if (Auxiliar::isWindows()==false) {
-				QAction *festival = menu->addAction(tr("Read"));
-				
-				language = getLanguage();
-				festival->setEnabled(m_festival_enable && language!=0);
+    virtual void contextMenuEvent(QContextMenuEvent *e) {
+        int language;
+        m_qpoint=e->pos();
 
-				connect(festival,SIGNAL(triggered()),m_parent,SLOT(FestivalExecuteDefinition()));
+        selectWord();
 
-			}
-			menu->exec(e->globalPos());
-		}
+        QMenu *menu = createStandardContextMenu();
+        if (Auxiliar::isWindows()==false) {
+            QAction *festival = menu->addAction(tr("Read"));
 
-		QPoint getPosition() {
-			return m_qpoint;
-		}
+            language = getLanguage();
+            festival->setEnabled(m_festival_enable && language!=0);
 
-		int getLanguage() {
-			int cursor_position=getCharPosition();
-			//return Auxiliar::catalan(), Auxiliar::english(),
-			//3: UI language
-			//4: destination language
-			//5: NOT destination language
-			
-			QString text = this->toPlainText();
+            connect(festival,SIGNAL(triggered()),m_parent,SLOT(FestivalExecuteDefinition()));
 
-			int cat = Auxiliar::catalan();
-			int eng = Auxiliar::english();
+        }
+        menu->exec(e->globalPos());
+    }
 
-			QHash<QString,int>hash;
+    QPoint getPosition() const {
+        return m_qpoint;
+    }
 
-			hash.insert(tr("Catalan example(s): "),cat);
-			hash.insert(tr("English example(s): "),eng);
-			hash.insert(tr("English notes: "),eng);
-			hash.insert(tr("Catalan notes: "),cat);
-			hash.insert(tr("Example(s): "),5);
-			hash.insert(tr("Synonym(s): "),5);
-			hash.insert(tr("Plural: "),4);
-			hash.insert(tr("Female Plural: "),4);
-		
-			hash.insert(tr("Picture: "),0);
-			hash.insert(tr("Flickr: "),0);
+    int getLanguage() const {
+        int cursor_position=getCharPosition();
+        //return Auxiliar::catalan(), Auxiliar::english(),
+        //3: UI language
+        //4: destination language
+        //5: NOT destination language
 
-			QHashIterator<QString,int> i(hash);
-			
-			int position=0;
-			int textLanguage=4;
-			int temp;
-			while (i.hasNext()) {
-				i.next();
-				temp=text.lastIndexOf(i.key(),cursor_position,Qt::CaseSensitive);
-				if (temp!=-1 && temp > position) {
-					position=temp;
-					textLanguage=i.value();
+        QString text = this->toPlainText();
 
-					if (temp+i.key().length() > cursor_position) {
-						textLanguage=3;
-					}
-				}
-			}
-			Auxiliar::debug("Language to read: "+textLanguage);
-			return textLanguage;
-		}
+        int cat = Auxiliar::catalan();
+        int eng = Auxiliar::english();
 
-		QString getText() {
-			QTextCursor tc = this->textCursor();
-			return tc.selectedText();
-		}
+        QHash<QString,int>hash;
 
-	signals:
-		void browserFailed();
-		void browserOpened();
+        hash.insert(tr("Catalan example(s): "),cat);
+        hash.insert(tr("English example(s): "),eng);
+        hash.insert(tr("English notes: "),eng);
+        hash.insert(tr("Catalan notes: "),cat);
+        hash.insert(tr("Example(s): "),5);
+        hash.insert(tr("Synonym(s): "),5);
+        hash.insert(tr("Plural: "),4);
+        hash.insert(tr("Female Plural: "),4);
+
+        hash.insert(tr("Picture: "),0);
+        hash.insert(tr("Flickr: "),0);
+
+        QHashIterator<QString,int> i(hash);
+
+        int position=0;
+        int textLanguage=4;
+        int temp;
+        while (i.hasNext()) {
+            i.next();
+            temp=text.lastIndexOf(i.key(),cursor_position,Qt::CaseSensitive);
+            if (temp!=-1 && temp > position) {
+                position=temp;
+                textLanguage=i.value();
+
+                if (temp+i.key().length() > cursor_position) {
+                    textLanguage=3;
+                }
+            }
+        }
+        Auxiliar::debug("Language to read: "+textLanguage);
+        return textLanguage;
+    }
+
+    QString getText() const {
+        QTextCursor tc = this->textCursor();
+        return tc.selectedText();
+    }
+
+Q_SIGNALS:
+    void browserFailed();
+    void browserOpened();
 };
 #endif
